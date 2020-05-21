2ebae23857805661b48ff96fa72aacfad1c5b419
unknown <dominicdemeterca@gmail.com>
2020-05-21 18:11:12 +0200

    domiNotes linked

--- src/app/main-container/admin/admin.component.ts
+++ src/app/main-container/admin/admin.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public goDomiNotes() {
+    window.location.href = `https://master.d14g6sukj7cmzo.amplifyapp.com/logged?token=${localStorage.token}`;
   }
-
 }
