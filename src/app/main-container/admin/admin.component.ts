cacb466c85e366e1a4b14919f9e6b992bcd25abb
unknown <dominicdemeterca@gmail.com>
2020-05-22 18:14:16 +0200

    asdf

--- src/app/main-container/admin/admin.component.ts
+++ src/app/main-container/admin/admin.component.ts
@@ -11,7 +11,7 @@ export class AdminComponent implements OnInit {
   ngOnInit(): void {}
 
   public goDomiNotes() {
-    window.location.href = `https://dominnotes.dev/?token=${localStorage.token}`;
+    window.location.href = `https://dominnotes.domid.dev/?token=${localStorage.token}`;
 
     //window.location.href = `http://localhost:4200/?token=${localStorage.token}`;
   }
